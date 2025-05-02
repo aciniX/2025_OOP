f1c51b9e43f4dbdc2bc907b399e9ed0088315cae
aciniX <acini@trinity.nsw.edu.au>
2025-05-02 12:42:03 +1000

    Refactor rectangle drawing logic and update background color

--- Intro/intro.py
+++ Intro/intro.py
@@ -1,7 +1,20 @@
 # importing the library
 import pygame
- 
-# initializing all the imported
+
+class DrawRect:
+    def __init__(self, surface, color, xPos, yPos, width, height):
+        self.surface = surface
+        self.color = color
+        self.xPos = xPos
+        self.yPos = yPos
+        self.width = width
+        self.height = height
+
+    def DrawShape(self):
+        pygame.draw.rect(self.surface, self.color, [self.xPos, self.yPos, self.width, self.height], 0)
+        # pygame.display.update()
+
+# initializing all the importe
 # pygame modules
 (numpass,numfail) = pygame.init()
  
@@ -16,7 +29,7 @@ screenHeight = 720
 pygame.display.set_mode((screenWidth, screenHeight))
 
 # Initializing RGB Color
-bgColor = (255, 0, 0)
+bgColor = (0, 0, 0)
 
 # Creating a reference to the surface
 surface = pygame.display.get_surface()
@@ -29,6 +42,8 @@ pygame.display.flip()  # render the bgcolor
 title = 'Intro to pygame'
 pygame.display.set_caption(title) 
 
+rect1 = DrawRect(surface, (0, 0, 255), 100, 100, 400, 100)
+rect2 = DrawRect(surface, (0, 255, 0), screenWidth, screenHeight, 200, 50)
 # creating a bool value which checks allows the game to run
 running = True
   
@@ -41,7 +56,17 @@ while running:
         # if event is of type quit then set running bool to false 
         if event.type == pygame.QUIT: 
             running = False
-        
+    
     # Changing surface color
     surface.fill(bgColor)  # setting acolor for the background
-    pygame.display.flip()  # render the bgcolor
+
+    # Using draw.rect module of
+    # pygame to draw the outlined rectangle
+    # (what we draw on, color of shape, size and position of shape, width of outline)
+    # pygame.draw.rect(surface, (0, 0, 255), [100, 100, 400, 100], 0)
+    rect1.DrawShape()
+    rect2.DrawShape()  
+    
+    #pygame.display.flip()  # render the bgcolor
+    pygame.display.update()  # update the display
+
